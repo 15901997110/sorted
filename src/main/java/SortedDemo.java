7dc520b0936ec0c5958076950a3a3ce97c23db4a
shaneAtHome <shane_lu@163.com>
2019-01-24 16:07:11 +0800

    Initial commit

---
+++ src/main/java/SortedDemo.java
@@ -0,0 +1,63 @@
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+
+public class SortedDemo {
+    private int[] arr = {23, 45, 3, 44, 99, 13, 33};
+
+    //冒泡排序
+    @Test
+    public void bubbleSort() {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
+    //选择排序
+    @Test
+    public void selectionSort() {
+        for (int i = 0; i < arr.length - 1; i++) {//从第一个元素开始，这个元素与数组后面的每个元素比较，选择最小的值
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    int temp = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
+    /**
+     * 插入排序
+     * 从第一个元素开始，该元素可以认为已经被排序；
+     * 取出下一个元素，在已经排序的元素序列中从后向前扫描；
+     * 如果该元素（已排序）大于新元素，将该元素移到下一位置；
+     * 重复步骤3，直到找到已排序的元素小于或者等于新元素的位置；
+     * 将新元素插入到该位置后；
+     * 重复步骤2~5。
+     */
+
+    @Test
+    public void insertionSort() {
+        int current;
+        for (int i = 0; i < arr.length - 1; i++) {
+            current = arr[i + 1];
+            int preIndex = i;
+            while (preIndex >= 0 && current < arr[preIndex]) {
+                arr[preIndex + 1] = arr[preIndex];
+                preIndex--;
+            }
+            arr[preIndex + 1] = current;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
